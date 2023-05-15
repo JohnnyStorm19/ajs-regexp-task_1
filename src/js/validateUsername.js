e1166de918ddceb77ad9fcbfbc771de14674727f
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2023-05-15 11:27:24 +0300

    changed regex names

--- src/js/validateUsername.js
+++ src/js/validateUsername.js
@@ -5,9 +5,9 @@ export default class Validator {
 
   validateUsername() {
     const name = String(this.userName);
-    const regex1 = /^[^\d\s_-][\w\s-]+[^\d\s_-]$/i;
-    const regex2 = /\d{4,}/;
-    if (regex1.test(name) && !regex2.test(name)) {
+    const regex = /^[^\d\s_-][\w\s-]+[^\d\s_-]$/i;
+    const exeptThreeNums = /\d{4,}/;
+    if (regex.test(name) && !exeptThreeNums.test(name)) {
       return true;
     }
     return false;
