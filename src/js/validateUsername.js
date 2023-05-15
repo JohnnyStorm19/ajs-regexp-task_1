a2caf72c0e4dfdd792989e35eed4f182d25b1bd8
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2023-05-15 13:19:26 +0300

    regex changed

--- src/js/validateUsername.js
+++ src/js/validateUsername.js
@@ -6,8 +6,8 @@ export default class Validator {
   validateUsername() {
     const name = String(this.userName);
     const regex = /^[^\d\s_-][\w\s-]+[^\d\s_-]$/i;
-    const exeptThreeNums = /\d{4,}/;
-    if (regex.test(name) && !exeptThreeNums.test(name)) {
+    const exeptThreeNumsInRow = /\d{4,}/;
+    if (regex.test(name) && !exeptThreeNumsInRow.test(name)) {
       return true;
     }
     return false;
